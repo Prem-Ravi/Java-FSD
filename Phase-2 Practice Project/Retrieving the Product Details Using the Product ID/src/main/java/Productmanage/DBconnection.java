67a45f3a3da2261594ce2df5352f0beb2a9707d6
Prem-Ravi <109294441+Prem-Ravi@users.noreply.github.com>
2022-08-09 16:41:58 +0530

    pp commit

---
+++ Phase-2 Practice Project/Retrieving the Product Details Using the Product ID/src/main/java/Productmanage/DBconnection.java
@@ -0,0 +1,13 @@
+package Productmanage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection {
+	static Connection con=null;
+	public static Connection getMyConnection() throws Exception {
+	Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root","mysql@28200");
+		return con;
+	}
+}
